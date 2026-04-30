f815cee73962235d0364a3b2448fe1c459290c2a
Poranut Chollavorn <pipech@gmail.com>
2026-04-30 20:38:49 +0700

    feat: add railway

---
+++ setup-railway-separated/script/railway-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+echo "-> Set ownership of sites folder"
+chown frappe:frappe /home/frappe/bench/sites
+
+echo "-> Linking assets"
+su frappe -c "ln -sf /home/frappe/bench/built_sites/assets /home/frappe/bench/sites/assets"
+su frappe -c "ln -sf /home/frappe/bench/built_sites/apps.json /home/frappe/bench/sites/apps.json"
+su frappe -c "ln -sf /home/frappe/bench/built_sites/apps.txt /home/frappe/bench/sites/apps.txt"
+
+exec "$@"
