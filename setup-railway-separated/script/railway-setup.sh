d8ddfd0c3ee7c952d6a21178812661b34ddba69f
Poranut Chollavorn <pipech@gmail.com>
2026-05-01 16:19:47 +0700

    fix: setup command

--- setup-railway-separated/script/railway-setup.sh
+++ setup-railway-separated/script/railway-setup.sh
@@ -20,7 +20,7 @@ if [ -d "${SITES_DIR}/${RFP_DOMAIN_NAME}" ]; then
 fi
 
 echo "-> Create empty common site config"
-su frappe -c 'echo "{}" > "${SITES_DIR}/common_site_config.json"'
+su frappe -c "echo '{}' > \"${SITES_DIR}/common_site_config.json\""
 
 echo "-> Create new site with ERPNext"
 su frappe -c "bench new-site ${RFP_DOMAIN_NAME} --admin-password ${RFP_SITE_ADMIN_PASSWORD} --no-mariadb-socket --db-root-password ${FRAPPE_DB_PASSWORD} --install-app erpnext"
