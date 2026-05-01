462b1881d074629b5a142203a63e12a47939e3f7
Poranut Chollavorn <pipech@gmail.com>
2026-05-01 11:31:21 +0700

    fix: start command

--- setup-railway-separated/script/railway-cmd.sh
+++ setup-railway-separated/script/railway-cmd.sh
@@ -18,7 +18,7 @@ su frappe -c "bench execute frappe.cache_manager.clear_global_cache"
 
 echo "-> Bursting env into config"
 envsubst '$RFP_DOMAIN_NAME' < /home/frappe/temp_nginx.conf > /etc/nginx/conf.d/default.conf
-envsubst '$PATH,$HOME,$NVM_DIR,$NODE_VERSION' < /home/frappe/temp_supervisor.conf > /home/frappe/supervisor.conf
+envsubst '$PATH,$HOME' < /home/frappe/temp_supervisor.conf > /home/frappe/supervisor.conf
 
 echo "-> Starting nginx"
 nginx
