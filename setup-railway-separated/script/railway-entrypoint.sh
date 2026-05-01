de698111fe0ac101fbe1d00c53b9963d7179dd0c
Poranut Chollavorn <pipech@gmail.com>
2026-05-01 10:52:58 +0700

    fix: entry point

--- setup-railway-separated/script/railway-entrypoint.sh
+++ setup-railway-separated/script/railway-entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 echo "-> Set ownership of sites folder"
 chown frappe:frappe /home/frappe/frappe-bench/sites
 
+echo "-> Set permission of site config"
+chown frappe:frappe /home/frappe/frappe-bench/sites/common_site_config.json
+
 echo "-> Linking assets"
 su frappe -c "ln -sf /home/frappe/frappe-bench/built_sites/assets /home/frappe/frappe-bench/sites/assets"
 su frappe -c "ln -sf /home/frappe/frappe-bench/built_sites/apps.json /home/frappe/frappe-bench/sites/apps.json"
